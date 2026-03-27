5f51069ace41703cd61b7a604e39d802761cb480
arminisa95 <arminisa@outlook.com>
2026-03-27 19:46:21 +0100

    Update database name to turm20_Armin_Imamovic

--- api/content.php
+++ api/content.php
@@ -10,7 +10,7 @@ header('Cache-Control: no-cache, must-revalidate');
 // Database config - UPDATE THESE
 $dbConfig = [
     'host' => 'mysqlsvr75.world4you.com',
-    'name' => 'turm20neu',
+    'name' => 'turm20_Armin_Imamovic',
     'user' => 'sql4460592',
     'pass' => 'xr+3bg03'
 ];
